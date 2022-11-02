5faeca855125b17d44d46bd76240018e679313c1
seungheonkim <95217370+seungheonkim@users.noreply.github.com>
2022-11-02 10:46:13 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-store
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-t8ku3l)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>{`Count: ${1}`}</h1>
+    </div>
+  );
+}
